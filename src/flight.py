d731b6448a8de33c7cb0c85b16de464786536208
Tiago Vicente <tmavicente@gmail.com>
2019-01-25 12:02:49 +0000

    merged

--- src/flight.py
+++ src/flight.py
@@ -17,39 +17,20 @@ $FP{callsign}{flight_plan}
   )
 
 def arrival(callsign, departure, destination, rwy):
-<<<<<<< HEAD
-  flight_plan, entry_point = choice(AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure])
-  position = AIRPORT_SETTINGS[destination]['ARRIVAL_POSITIONS'][entry_point]
-  expected_alt = AIRPORT_SETTINGS[destination]['ARRIVAL_STARS'][rwy][departure]
-  route = choice(AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][entry_point])
-  start_delay = random.randint(0,30)
-=======
   flight_plan, position = choice(AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure])
   route, position_coords, expected_alt = AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][position]
->>>>>>> random-arrivals2
 
   return position_coords, '''
 @N:{callsign}:0000:1:{position_coords}:32000:0:50:0:0
 $FP{callsign}{flight_plan}
 $ROUTE:{route}
-<<<<<<< HEAD
-START:{start_delay}
-DELAY:2:5
-REQALT:{expected_alt}
-=======
 DELAY:3:7
 REQALT:{position}:{expected_alt}
->>>>>>> random-arrivals2
 '''.format(
     callsign=callsign,
     position_coords=position_coords,
     flight_plan=flight_plan,
     route=route,
-<<<<<<< HEAD
-    expected_alt=expected_alt,
-	start_delay=start_delay
-=======
     position=route.split()[0],
 	  expected_alt=expected_alt
->>>>>>> random-arrivals2
   )
