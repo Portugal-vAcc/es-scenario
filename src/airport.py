eab8af3f9dae89e5dd0abaf0ceb9419e6d773d31
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-01-27 20:11:36 +0000

    Clean changes

--- src/airport.py
+++ src/airport.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 """
 Euroscope Sweatbox Scenario Maker
+
 Copyright (C) 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
                     Tiago Vicente <tmavicente@gmail.com>
+
 This file is part of Euroscope Sweatbox Scenario Maker.
+
 Euroscope Sweatbox Scenario Maker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 2 of the License.
+
 Euroscope Sweatbox Scenario Maker is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with Euroscope Sweatbox Scenario Maker. If not, see <http://www.gnu.org/licenses/>.
 """
@@ -22,11 +27,13 @@ from . import flight
 def make_scenario(icao, rwy, total_departures, total_arrivals):
     """Generates a Euroscope Sweatbox scenario for the given icao and active rwy, with random
     arrivals and departures.
+
     Args:
         icao (str): The scenario ICAO airfield code.
         rwy (str): Runway in use.
         total_departures (int, optional): Total departing aircraft.
         total_arrivals (int, optional): The scenario ICAO airfield code.
+
     Returns:
         list: A list of str containing the scenario lines to feed to the output file.
     """
@@ -43,8 +50,11 @@ def make_scenario(icao, rwy, total_departures, total_arrivals):
 
 def _generate_departures(icao, rwy):
     """Generates a random list of departure flights from a list of flights.
+
     Args:
         icao (str): The scenario ICAO airfield code.
+        rwy (str): The runway in use.
+
     Returns:
         list: A list of str containing the scenario lines to feed to the output file.
     """
@@ -71,9 +81,11 @@ def _generate_departures(icao, rwy):
 
 def _generate_arrivals(icao, rwy):
     """Generates a random list of arrival flights from a list of flights.
+
     Args:
         icao (str): The scenario ICAO airfield code.
         rwy (str): The runway in use.
+
     Returns:
         list: A list of str containing the scenario lines to feed to the output file.
     """
@@ -92,4 +104,3 @@ def _generate_arrivals(icao, rwy):
     shuffle(arrivals)
 
     return arrivals
-    
--- src/flight.py
+++ src/flight.py
@@ -25,12 +25,14 @@ from . import AIRPORT_SETTINGS
 
 def make_departure(callsign, departure, destination, stand, rwy):
     """Generates a random departure flight from a list of flights.
+
     Args:
         callsign (str): The aircraft callsign.
         departure (str): The ICAO code for the departing airfield
             (ie. the airport we're simulating).
         destination (str): The ICAO code for the flight destination airfield.
         stand (str): The available stand where the flight should be placed.
+
     Returns:
         str: The flight string to be feed into the scenario file.
     """
@@ -53,13 +55,13 @@ $FP{callsign}{flight_plan}{route}
 $ROUTE:{sidroute}
 REQALT:{sidfix}:{expected_alt}
 '''.format(
-    callsign=callsign,
-    stand=stand,
-    flight_plan=flight_plan,
-    route=route,
-    sidroute=sidroute,
-    sidfix=sidfix,
-    expected_alt=expected_alt
+        callsign=callsign,
+        stand=stand,
+        flight_plan=flight_plan,
+        route=route,
+        sidroute=sidroute,
+        sidfix=sidfix,
+        expected_alt=expected_alt
     )
 
 def make_arrival(callsign, departure, destination, rwy):
@@ -73,42 +75,37 @@ def make_arrival(callsign, departure, destination, rwy):
     Returns:
         str: The flight string to be feed into the scenario file.
     """
-    flight_plan_route, position, level, fp_direction = choice(
-        AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure]
-        )
-    (route,
-     position_coords,
-     expected_alt) = AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][position]
-    random_altitude = choice(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
-    flight_plan = '''
-:*A:I:B738:364:{departure}:0000:0000:{random_altitude}:{destination}:00:00:0:0::/v/:
-'''.format(
-    departure=departure,
-    random_altitude=random_altitude,
-    destination=destination
-    )
+    fpl_data = AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure]
+    flight_plan_route, position, level, fp_direction = choice(fpl_data)
 
-    #Preve planos de voo inferiores à altitude de cruzeiro para LOW_LEVEL FP's
-    if (random_altitude < expected_alt and level == 'LOW_LEVEL'):
-        expected_alt = random_altitude
-    #Prever a mesma situação para HIGH_LEVEL
-    if (random_altitude < expected_alt and level == 'HIGH_LEVEL'):
-        random_altitude = max(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
+    arrival_route_data = AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][position]
+    route, position_coords, expected_alt = arrival_route_data
 
+    altitude = choice(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
 
-    return position_coords, '''
-@N:{callsign}:0000:1:{position_coords}:{random_altitude}:0:50:0:0
-$FP{callsign}{flight_plan}{flight_plan_route}
+    below_star_profile = altitude < expected_alt
+    if below_star_profile:
+        if level == 'LOW_LEVEL':
+            # ignore star descend profile
+            expected_alt = altitude
+        if level == 'HIGH_LEVEL':
+            # use a higher cruise level
+            altitude = max(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
+
+    return position_coords, '''\
+@N:{callsign}:0000:1:{position_coords}:{altitude}:0:50:0:0
+$FP{callsign}:*A:I:B738:364:{departure}:0000:0000:{altitude}:{destination}:00:00:0:0::/v/:{flight_plan_route}
 $ROUTE:{route}
 DELAY:3:7
 REQALT:{position}:{expected_alt}
 '''.format(
-    callsign=callsign,
-    position_coords=position_coords,
-    flight_plan=flight_plan,
-    flight_plan_route=flight_plan_route,
-    route=route,
-    position=route.split()[0],
-    expected_alt=expected_alt,
-    random_altitude=random_altitude
+        callsign=callsign,
+        position_coords=position_coords,
+        flight_plan_route=flight_plan_route,
+        route=route,
+        position=route.split()[0],
+        expected_alt=expected_alt,
+        altitude=altitude,
+        departure=departure,
+        destination=destination
     )
