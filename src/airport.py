c4e541f9ff38439b6019459aa02fb689c092dc54
Tiago Vicente <tmavicente@gmail.com>
2019-01-27 15:40:06 +0000

    fuck linter to airport.py

--- src/airport.py
+++ src/airport.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
 """
 Euroscope Sweatbox Scenario Maker
-
 Copyright (C) 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
                     Tiago Vicente <tmavicente@gmail.com>
-
 This file is part of Euroscope Sweatbox Scenario Maker.
-
 Euroscope Sweatbox Scenario Maker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 2 of the License.
-
 Euroscope Sweatbox Scenario Maker is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with Euroscope Sweatbox Scenario Maker. If not, see <http://www.gnu.org/licenses/>.
 """
 from random import shuffle
-from collections import defaultdict
 
 from . import AIRPORT_SETTINGS
 from . import flight
@@ -28,13 +22,11 @@ from . import flight
 def make_scenario(icao, rwy, total_departures, total_arrivals):
     """Generates a Euroscope Sweatbox scenario for the given icao and active rwy, with random
     arrivals and departures.
-
     Args:
         icao (str): The scenario ICAO airfield code.
         rwy (str): Runway in use.
         total_departures (int, optional): Total departing aircraft.
         total_arrivals (int, optional): The scenario ICAO airfield code.
-
     Returns:
         list: A list of str containing the scenario lines to feed to the output file.
     """
@@ -51,10 +43,8 @@ def make_scenario(icao, rwy, total_departures, total_arrivals):
 
 def _generate_departures(icao, rwy):
     """Generates a random list of departure flights from a list of flights.
-
     Args:
         icao (str): The scenario ICAO airfield code.
-
     Returns:
         list: A list of str containing the scenario lines to feed to the output file.
     """
@@ -65,61 +55,41 @@ def _generate_departures(icao, rwy):
     shuffle(stands)
 
     for destination in callsigns_per_destination:
-      for callsign in callsigns_per_destination[destination]:
-        if (len(stands) > 0):
-          departures.append(flight.departure(
-            callsign,
-            icao,
-            destination,
-            stands.pop(),
-			rwy
-          ))
+        for callsign in callsigns_per_destination[destination]:
+            if stands:
+                departures.append(flight.make_departure(
+                    callsign,
+                    icao,
+                    destination,
+                    stands.pop(),
+                    rwy
+                ))
 
     shuffle(departures)
 
-    def get_0():
-      return 0
-    counter = defaultdict(get_0)
-    d = []
-    for spawn, departure in departures:
-      d.append(departure + 'START:%s' % (counter[spawn] * 1))
-      counter[spawn] = counter[spawn] + 1
-    departures = d
+    return departures
 
 def _generate_arrivals(icao, rwy):
+    """Generates a random list of arrival flights from a list of flights.
+    Args:
+        icao (str): The scenario ICAO airfield code.
+        rwy (str): The runway in use.
+    Returns:
+        list: A list of str containing the scenario lines to feed to the output file.
+    """
     arrivals = list()
     callsigns_per_departure = AIRPORT_SETTINGS[icao]['ARRIVAL_CALLSIGNS']
 
     for departure in callsigns_per_departure:
-      for callsign in callsigns_per_departure[departure]:
-        arrivals.append(flight.arrival(
-          callsign,
-          departure,
-          icao,
-          rwy
-        ))
+        for callsign in callsigns_per_departure[departure]:
+            arrivals.append(flight.make_arrival(
+                callsign,
+                departure,
+                icao,
+                rwy
+            ))
 
     shuffle(arrivals)
 
-    def get_0():
-      return 0
-    counter = defaultdict(get_0)
-    a = []
-    for spawn, arrival in arrivals:
-      a.append(arrival + 'START:%s' % (counter[spawn] * 4))
-      counter[spawn] = counter[spawn] + 1
-    arrivals = a
-
-
-
-def __str__(self):
-    return '\n'.join([
-      *AIRPORT_SETTINGS[icao][rwy],
-	  '\n',
-      *AIRPORT_SETTINGS[icao]['ALL'],
-	  '\n',
-	  *AIRPORT_SETTINGS[icao]['HOLDS'][rwy],
-      '\n',
-      *departures[:total_departures],
-      *arrivals[:total_arrivals]
-    ])
+    return arrivals
+    
