635d636f07612330dbfd25dbeb5e7707bf85818e
Tiago Vicente <tmavicente@gmail.com>
2019-01-27 03:02:28 +0000

    test fix conflits

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,16 @@
-AIRPORT_SETTINGS = {
-  'LPPR': _LPPR,
-  'LPCS': _LPCS,
-  'LPPT': _LPPT,
-  'LPFR': _LPFR,
-  'LPMA': _LPMA,
-}
-=======
+#!/usr/bin/env python3
+"""
+Euroscope Sweatbox Scenario Maker
+Copyright (C) 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
+                    Tiago Vicente <tmavicente@gmail.com>
+This file is part of Euroscope Sweatbox Scenario Maker.
+Euroscope Sweatbox Scenario Maker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 2 of the License.
 Euroscope Sweatbox Scenario Maker is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with Euroscope Sweatbox Scenario Maker. If not, see <http://www.gnu.org/licenses/>.
 """
