99cd9f96afda4829bcb331083e04b4ca36a9d84e
Tiago Vicente <tmavicente@gmail.com>
2019-01-27 15:24:26 +0000

    fuck linter to flight.py

--- src/flight.py
+++ src/flight.py
@@ -24,7 +24,7 @@ from random import choice
 from . import AIRPORT_SETTINGS
 
 def make_departure(callsign, departure, destination, stand, rwy):
-  """Generates a random departure flight from a list of flights.
+    """Generates a random departure flight from a list of flights.
     Args:
         callsign (str): The aircraft callsign.
         departure (str): The ICAO code for the departing airfield
@@ -33,13 +33,21 @@ def make_departure(callsign, departure, destination, stand, rwy):
         stand (str): The available stand where the flight should be placed.
     Returns:
         str: The flight string to be feed into the scenario file.
-  """
-  route, level, fp_direction, sidfix = choice(AIRPORT_SETTINGS[departure]['DEPARTURE_FPL'][destination])
-  random_altitude = choice(AIRPORT_SETTINGS[departure]['GET_FL'][fp_direction][level])
-  flight_plan = ':*A:I:B738:400:'+departure+':0000:0000:'+random_altitude+':'+destination+':00:00:0:0::/v/:'
-  sidroute, expected_alt = AIRPORT_SETTINGS[departure]['DEPARTURE_ROUTES'][rwy][sidfix]
+    """
+    route, level, fp_direction, sidfix = choice(
+        AIRPORT_SETTINGS[departure]['DEPARTURE_FPL'][destination]
+        )
+    random_altitude = choice(AIRPORT_SETTINGS[departure]['GET_FL'][fp_direction][level])
+    flight_plan = '''
+:*A:I:B738:400:{departure}:0000:0000:{random_altitude}:{destination}:00:00:0:0::/v/:
+'''.format(
+    departure=departure,
+    random_altitude=random_altitude,
+    destination=destination
+    )
+    sidroute, expected_alt = AIRPORT_SETTINGS[departure]['DEPARTURE_ROUTES'][rwy][sidfix]
 
-  return sidfix, '''
+    return sidfix, '''
 @N:{callsign}:2200:1:{stand}:0:0:0:0:0
 $FP{callsign}{flight_plan}{route}
 $ROUTE:{sidroute}
@@ -48,14 +56,14 @@ REQALT:{sidfix}:{expected_alt}
     callsign=callsign,
     stand=stand,
     flight_plan=flight_plan,
-	route=route,
-	sidroute=sidroute,
-	sidfix=sidfix,
-	expected_alt=expected_alt
-  )
+    route=route,
+    sidroute=sidroute,
+    sidfix=sidfix,
+    expected_alt=expected_alt
+    )
 
 def make_arrival(callsign, departure, destination, rwy):
-  """Generates a random arrival flight from a list of flights.
+    """Generates a random arrival flight from a list of flights.
     Args:
         callsign (str): The aircraft callsign.
         departure (str): The ICAO code for the departing airfield.
@@ -64,21 +72,31 @@ def make_arrival(callsign, departure, destination, rwy):
         rwy (str): The runway in use.
     Returns:
         str: The flight string to be feed into the scenario file.
-  """
-  flight_plan_route, position, level, fp_direction = choice(AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure])
-  route, position_coords, expected_alt = AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][position]
-  random_altitude = choice(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
-  flight_plan = ':*A:I:B738:364:'+departure+':0000:0000:'+random_altitude+':'+destination+':00:00:0:0::/v/:'
+    """
+    flight_plan_route, position, level, fp_direction = choice(
+        AIRPORT_SETTINGS[destination]['ARRIVAL_FPL'][departure]
+        )
+    (route,
+     position_coords,
+     expected_alt) = AIRPORT_SETTINGS[destination]['ARRIVAL_ROUTES'][rwy][position]
+    random_altitude = choice(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
+    flight_plan = '''
+:*A:I:B738:364:{departure}:0000:0000:{random_altitude}:{destination}:00:00:0:0::/v/:
+'''.format(
+    departure=departure,
+    random_altitude=random_altitude,
+    destination=destination
+    )
 
-  #Preve planos de voo inferiores à altitude de cruzeiro para LOW_LEVEL FP's
-  if (random_altitude < expected_alt and level == 'LOW_LEVEL'):
-	  expected_alt = random_altitude
-  #Prever a mesma situação para HIGH_LEVEL
-  if (random_altitude < expected_alt and level == 'HIGH_LEVEL'):
-	  random_altitude = max(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
+    #Preve planos de voo inferiores à altitude de cruzeiro para LOW_LEVEL FP's
+    if (random_altitude < expected_alt and level == 'LOW_LEVEL'):
+        expected_alt = random_altitude
+    #Prever a mesma situação para HIGH_LEVEL
+    if (random_altitude < expected_alt and level == 'HIGH_LEVEL'):
+        random_altitude = max(AIRPORT_SETTINGS[destination]['GET_FL'][fp_direction][level])
 
 
-  return position_coords, '''
+    return position_coords, '''
 @N:{callsign}:0000:1:{position_coords}:{random_altitude}:0:50:0:0
 $FP{callsign}{flight_plan}{flight_plan_route}
 $ROUTE:{route}
@@ -88,9 +106,9 @@ REQALT:{position}:{expected_alt}
     callsign=callsign,
     position_coords=position_coords,
     flight_plan=flight_plan,
-	flight_plan_route=flight_plan_route,
+    flight_plan_route=flight_plan_route,
     route=route,
     position=route.split()[0],
     expected_alt=expected_alt,
-	random_altitude=random_altitude
-  )
+    random_altitude=random_altitude
+    )
