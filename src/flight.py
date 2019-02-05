5d340bd6315b6f92ebe47792d4a395128d32b82c
pikachu1992 <tmavicente@gmail.com>
2019-02-05 19:42:34 +0000

    Update flight.py

--- src/flight.py
+++ src/flight.py
@@ -41,9 +41,7 @@ def make_departure(callsign, departure, destination, stand, rwy):
         AIRPORT_SETTINGS[departure]['DEPARTURE_FPL'][destination]
         )
     random_altitude = choice(AIRPORT_SETTINGS[departure]['GET_FL'][fp_direction][level])
-    flight_plan = '''\
-<<<<<<< HEAD
-:*A:I:B738:400:{departure}:0000:0000:{random_altitude}:{destination}:00:00:0:0::/v/:'''.format(
+    flight_plan = ''':*A:I:B738:400:{departure}:0000:0000:{random_altitude}:{destination}:00:00:0:0::/v/:'''.format(
         departure=departure,
         random_altitude=random_altitude,
         destination=destination
